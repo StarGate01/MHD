686d09d1edbf394767ac4944fa6ba831c8d2bc51
StarGate01 <christoph.honal@web.de>
2014-07-25 21:19:54 +0200

    Editor prepare

---
+++ MHD/Content/Level/LevelScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MHD.Content.Level
+{
+
+    public interface LevelScript
+    {
+
+        Root GetData();
+      
+        Gameplay.Objects.GameObject GetObject(string UID);
+
+    }
+
+}
